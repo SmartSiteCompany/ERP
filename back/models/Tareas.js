a6e148a3ab34bec04665f775fea6a45cf97a84cb
EIvanCQuiroz <al222111295@gmail.com>
2025-04-11 11:32:10 -0600

    Agregando Filial y usuario al modelo Tarea

--- back/models/Tareas.js
+++ back/models/Tareas.js
@@ -2,7 +2,9 @@
 const mongoose = require('mongoose');
 
 const tareaSchema = new mongoose.Schema({
-  cliente_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Cliente', required: true },
+  cliente_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Cliente', required: false},
+  usuario_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
+  filial_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Filial', required: true },
   descripcion: { type: String, required: true },
   fecha_vencimiento: { type: Date, required: true },
   estado: { type: String, enum: ['pendiente', 'en progreso', 'completada'], default: 'pendiente' },

ad689edad4eec2614b40c095cdd6766e8482f6b3
EIvanCQuiroz <al222111295@gmail.com>
2025-04-14 11:33:55 -0600

    Agregando fecha de inicio a Tarea

--- back/models/Tareas.js
+++ back/models/Tareas.js
@@ -6,8 +6,8 @@ const tareaSchema = new mongoose.Schema({
   usuario_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
   filial_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Filial', required: true },
   descripcion: { type: String, required: true },
-  fecha_creacion: { type: Date, default: Date.now },
-  fecha_vencimiento: { type: Date, required: true },
+  fecha_creacion: { type: Date, default: Date.now, required: true },
+  fecha_vencimiento: { type: Date, default: Date.now, required: true },
   estado: { type: String, enum: ['pendiente', 'en progreso', 'completada'], default: 'pendiente' },
   responsable: { type: String, required: true },
 });

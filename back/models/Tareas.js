930da2ba87a57f8e46d2b9ed5ce1d2d301482bc2
EIvanCQuiroz <al222111295@gmail.com>
2025-04-14 10:41:52 -0600

    Agregando fecha a modelo Tarea

--- back/models/Tareas.js
+++ back/models/Tareas.js
@@ -6,6 +6,7 @@ const tareaSchema = new mongoose.Schema({
   usuario_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
   filial_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Filial', required: true },
   descripcion: { type: String, required: true },
+  fecha_creacion: { type: Date, default: Date.now },
   fecha_vencimiento: { type: Date, required: true },
   estado: { type: String, enum: ['pendiente', 'en progreso', 'completada'], default: 'pendiente' },
   responsable: { type: String, required: true },

cf4726b1c7c809600296f7c3adfedcd50784b1b2
Gibran Flores <gibran.floresmendieta@gmail.com>
2025-04-14 15:25:04 -0600

    agreagar la libreria axios y mongosee

--- ToDoTask/src/main.jsx
+++ ToDoTask/src/main.jsx
@@ -22,7 +22,7 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode> 
-      <MainPage />
+      <MainPage/>
     </StrictMode>
   </BrowserRouter>
 )
